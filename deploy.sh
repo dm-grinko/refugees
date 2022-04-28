e59ca5cb230236066a5373510ba6f2f07039d1b1
Dmitry Grinko <dm.grinko@gmail.com>
2022-04-27 17:23:13 -0700

    update

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,6 @@ rm -rf ./dist
 
 npm run build
 
-# cd ./dist
-
-# sed -i '' 's/old/new/g' index.html
-# sed -i '' 's/base href=\"/base href=\"\/refugees/g' index.html
-
-# sed -i '' 's/src=\"/src=\"\/refugees\//g' index.html
-# sed -i '' 's/href=\"styles/href=\"\/refugees\/styles/g' index.html
-
 git add .
 
 git commit -m update
@@ -21,6 +13,8 @@ git status
 
 git push origin master
 
-git push heroku master
+
 # heroku login
 # heroku git:remote -a refugees2022
+
+git push heroku master
--- generator.sh
+++ generator.sh
@@ -17,3 +17,8 @@
 # ng g c work/work
 
 
+# sed -i '' 's/old/new/g' index.html
+# sed -i '' 's/base href=\"/base href=\"\/refugees/g' index.html
+
+# sed -i '' 's/src=\"/src=\"\/refugees\//g' index.html
+# sed -i '' 's/href=\"styles/href=\"\/refugees\/styles/g' index.html
